10d191f70932a239137e34af901075bf6a2be94f
Amogh Joshi <a@as-MacBook-Pro.local>
2019-04-04 04:48:09 -0400

    Update game.js

--- src/game.js
+++ src/game.js
@@ -1,38 +1,130 @@
-Game = function(game) {}
+var score = 0;
+var scoreText;
 
-Game.prototype = {
-    preload: function() {
-        //load assets
-        this.game.load.image('circle','asset/circle.png');
-    	this.game.load.image('background', 'asset/tile.png');
-    },
-    create: function() {
-        var width = this.game.width;
-        var height = this.game.height;
-
-        this.game.world.setBounds(-width, -height, width*2, height*2);
-    	this.game.stage.backgroundColor = '#444';
-
-        //add tilesprite background
-        var background = this.game.add.tileSprite(-width, -height,
-            this.game.world.width, this.game.world.height, 'background');
-
-        //initialize physics and groups
-        this.game.physics.startSystem(Phaser.Physics.P2JS);
-
-        this.game.snakes = [];
-
-        //create player
-        var snake = new Snake(this.game, 'circle', 0, 0);
-        this.game.camera.follow(snake.head);
-    },
-    /**
-     * Main update loop
-     */
-    update: function() {
-        //update game components
-        for (var i = this.game.snakes.length - 1 ; i >= 0 ; i--) {
-            this.game.snakes[i].update();
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
         }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
     }
 };
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('sky', 'asset/sky.png');
+    this.load.image('ground', 'asset/platform.png');
+    this.load.image('star', 'asset/circle.png');
+    this.load.image('bomb', 'asset/white-shadow.png');
+    this.load.spritesheet('dude',
+        'asset/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+}
+
+
+
+function create ()
+{
+
+      this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+player = this.physics.add.sprite(100, 450, 'dude');
+
+player.setBounce(0.2);
+player.setCollideWorldBounds(true);
+
+this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+});
+
+this.anims.create({
+    key: 'turn',
+    frames: [ { key: 'dude', frame: 4 } ],
+    frameRate: 20
+});
+
+this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+});
+
+this.physics.add.collider(player, platforms);
+
+stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
+});
+
+stars.children.iterate(function (child) {
+
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+
+}
+);
+this.physics.add.collider(stars, platforms);
+
+this.physics.add.overlap(player, stars, collectStar, null, this);
+scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+}
+
+function collectStar (player, star)
+{
+    star.disableBody(true, true);
+     score += 10;
+    scoreText.setText('Score: ' + score);
+}
+
+function update ()
+{
+    cursors = this.input.keyboard.createCursorKeys();
+    if (cursors.left.isDown)
+{
+    player.setVelocityX(-160);
+
+    player.anims.play('left', true);
+}
+else if (cursors.right.isDown)
+{
+    player.setVelocityX(160);
+
+    player.anims.play('right', true);
+}
+else
+{
+    player.setVelocityX(0);
+
+    player.anims.play('turn');
+}
+
+if (cursors.up.isDown && player.body.touching.down)
+{
+    player.setVelocityY(-330);
+}
+}
